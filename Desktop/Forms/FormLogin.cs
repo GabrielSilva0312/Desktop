7fba967098333eaf78861aaf27e8c57cf1d6432d
Gabriel <gabrielpacheco625@gmail.com>
2022-02-02 08:11:20 -0300

    Ajustes

--- Desktop/Forms/FormLogin.cs
+++ Desktop/Forms/FormLogin.cs
@@ -23,7 +23,6 @@ namespace Desktop
 
         private void FormLogin_Load(object sender, EventArgs e)
         {
-            //
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
